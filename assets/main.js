f1cc199128b855b26882a437c23e271b2722e98d
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-09-20 01:09:31 +0700

    add number 2

--- assets/main.js
+++ assets/main.js
@@ -80,6 +80,10 @@ function main() {
 
   var xAxisVertices = new Float32Array([1.0, 0.0, -1.0, 0.0]);
   var yAxisVertices = new Float32Array([0.0, 1.0, 0.0, -1.0]);
+  var twoVertices = new Float32Array([
+    -0.25, 0.15, -0.7, 0.15, -0.7, 0.2, -0.75, 0.2, -0.75, 0.45, -0.7, 0.45, -0.7, 0.5, -0.65, 0.5, -0.65, 0.55, -0.4, 0.55, -0.4, 0.7, -0.6, 0.7, -0.6, 0.65, -0.75, 0.65, -0.75, 0.8, -0.7, 0.8, -0.7, 0.85, -0.3, 0.85, -0.3, 0.8, -0.25,
+    0.8, -0.25, 0.45, -0.3, 0.45, -0.3, 0.4, -0.55, 0.4, -0.55, 0.35, -0.6, 0.35, -0.6, 0.3, -0.25, 0.3, -0.25, 0.15,
+  ]);
 
   gl.clearColor(0.78, 0.078, 0.102, 1.0);
 
@@ -87,4 +91,5 @@ function main() {
 
   drawA(gl.LINES, xAxisVertices);
   drawA(gl.LINES, yAxisVertices);
+  drawA(gl.LINE_STRIP, twoVertices);
 }
