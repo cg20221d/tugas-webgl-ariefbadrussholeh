b022a19aa62f395278382feb7f12562be5562661
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2022-12-07 14:04:18 +0700

    Finish Soal 5 & 6: Hapus salah satu objek

--- Tugas_WebGL3_Retaston/index.js
+++ Tugas_WebGL3_Retaston/index.js
@@ -1,7 +1,5 @@
-import { position_vertices_e, color_vertices_e, indices_e } from "./libs/vertices/vertices_e.js";
 import { position_vertices_h, color_vertices_h, indices_h } from "./libs/vertices/vertices_h.js";
 import { position_vertices_2, color_vertices_2, indices_2 } from "./libs/vertices/vertices_2.js";
-import { position_vertices_8, color_vertices_8, indices_8 } from "./libs/vertices/vertices_8.js";
 import { position_vertices_box, color_vertices_box, indices_box } from "./libs/vertices/vertices_box.js";
 
 (function (global) {
@@ -60,50 +58,6 @@ import { position_vertices_box, color_vertices_box, indices_box } from "./libs/v
 
       var scale = 0.4;
       mat4.scale(model, model, [scale, scale, scale]);
-      mat4.rotateY(model, model, yTheta);
-
-      state.gl.uniformMatrix4fv(uModel, false, model);
-      state.gl.uniformMatrix4fv(uView, false, state.view);
-      state.gl.uniformMatrix4fv(uProjection, false, state.perspective);
-
-      state.gl.drawElements(state.gl.TRIANGLES, n, state.gl.UNSIGNED_BYTE, 0);
-    };
-  }
-
-  function LetterE() {
-    this.attributes = {
-      aColor: {
-        size: 3,
-        offset: 0,
-        bufferData: new Float32Array(color_vertices_e),
-      },
-      aPosition: {
-        size: 3,
-        offset: 0,
-        bufferData: new Float32Array(position_vertices_e),
-      },
-    };
-    this.indices = new Uint8Array(indices_e);
-    this.state = {
-      mm: mat4.create(),
-      nm: null,
-    };
-    this.stride = 0;
-
-    this.draw = function () {
-      var uModel = state.gl.getUniformLocation(state.programs[state.program], "uModel");
-      var uView = state.gl.getUniformLocation(state.programs[state.program], "uView");
-      var uProjection = state.gl.getUniformLocation(state.programs[state.program], "uProjection");
-
-      var model = mat4.create();
-
-      state.programs[state.program].renderBuffers(this);
-
-      var n = this.indices.length;
-
-      var scale = 3.0;
-      mat4.scale(model, model, [scale, scale, scale]);
-      mat4.translate(model, model, [-1.0, -1.0, 0.0]);
       mat4.rotateY(model, model, yTheta);
 
       state.gl.uniformMatrix4fv(uModel, false, model);
@@ -198,54 +152,6 @@ import { position_vertices_box, color_vertices_box, indices_box } from "./libs/v
         horizontalDelta += horizontalSpeed;
       }
       mat4.translate(model, model, [horizontalDelta, 1.0, 0.0]);
-      mat4.rotateY(model, model, y2Theta);
-      mat4.rotateX(model, model, x2Theta);
-
-      state.gl.uniformMatrix4fv(uModel, false, model);
-      state.gl.uniformMatrix4fv(uView, false, state.view);
-      state.gl.uniformMatrix4fv(uProjection, false, state.perspective);
-
-      state.gl.drawElements(state.gl.TRIANGLES, n, state.gl.UNSIGNED_BYTE, 0);
-    };
-  }
-
-  function Number8() {
-    this.attributes = {
-      aColor: {
-        size: 3,
-        offset: 0,
-        bufferData: new Float32Array(color_vertices_8),
-      },
-      aPosition: {
-        size: 3,
-        offset: 0,
-        bufferData: new Float32Array(position_vertices_8),
-      },
-    };
-    this.indices = new Uint8Array(indices_8);
-    this.state = {
-      mm: mat4.create(),
-      nm: null,
-    };
-    this.stride = 0;
-
-    this.draw = function () {
-      var uModel = state.gl.getUniformLocation(state.programs[state.program], "uModel");
-      var uView = state.gl.getUniformLocation(state.programs[state.program], "uView");
-      var uProjection = state.gl.getUniformLocation(state.programs[state.program], "uProjection");
-
-      var model = mat4.create();
-
-      state.programs[state.program].renderBuffers(this);
-
-      var n = this.indices.length;
-
-      if (!freeze) {
-        if (scaleDelta >= 2.0) scaleSpeed *= -1;
-        if (scaleDelta < 0.5) scaleSpeed *= -1;
-        scaleDelta += scaleSpeed;
-      }
-      mat4.scale(model, model, [scaleDelta, scaleDelta, scaleDelta]);
       mat4.rotateY(model, model, y2Theta);
       mat4.rotateX(model, model, x2Theta);
 
